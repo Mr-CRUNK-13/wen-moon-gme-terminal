d2c138ff3990a0b5a00ae53015c4ee82f98f0117
Mr-CRUNK-13 <mrfab13@gmail.com>
2026-03-17 14:47:55 +0100

    Update WEN_MOON_GME_TERMINAL_TEST.py

--- WEN_MOON_GME_TERMINAL_TEST.py
+++ WEN_MOON_GME_TERMINAL_TEST.py
@@ -1699,7 +1699,7 @@ else:
                     width: 100%;
                     border-collapse: collapse;
                     font-family: monospace;
-                    font-size: 14px;
+                    font-size: 18px;
                     text-align: center;
                     color: #FFFFFF;
                 }
