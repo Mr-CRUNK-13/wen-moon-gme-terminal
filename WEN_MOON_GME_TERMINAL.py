cf651438b4c29bb0e3c035ede9995640f35459f2
Mr-CRUNK-13 <mrfab13@gmail.com>
2026-03-26 11:56:57 +0100

    Update WEN_MOON_GME_TERMINAL.py

--- WEN_MOON_GME_TERMINAL.py
+++ WEN_MOON_GME_TERMINAL.py
@@ -1646,7 +1646,7 @@ else:
                     marker_color='#9b51e0',
                     text=[f"{v}M" for v in drs_values],
                     textposition='auto',
-                    textfont=dict(size=18),
+                    textfont=dict(size=30),
                     name="DRS Shares (Millions)"
                 ))
                 
